8f9bad83567b6082b58f06c4396b80c469aedcad
Kleber Coelho <kleber.amg79@gmail.com>
2020-09-10 13:45:59 -0300

    update tcp.py

--- tcp.py
+++ tcp.py
@@ -12,8 +12,21 @@ def bcd2int(bcd):
     ret = ret * 10 + digit
     return ret
 
+def checksum(frameHex):
+    tamFrameHex = len(frameHex)
+    frameEnvioHex = frameHex
+    frameHex.append(0)
 
-#--- opcao 12
+
+    for i in range(tamFrameHex):
+        print(i)
+        frameEnvioHex[tamFrameHex] += frameHex[i]
+
+
+    return frameEnvioHex[tamFrameHex]
+
+
+#--- Ler Data e Hora
 def opcao12(data):
 
     print ("received data:", data)
@@ -35,24 +48,32 @@ def opcao12(data):
 
 # Conexao TCP
 
-#TCP_IP = '10.238.10.108'
-TCP_IP = '10.238.0.41'
+def enviarComando(comando):
+    FrameHex = bytearray(comando)
+    print("soma checksum: ",sum(comando[:-1]))
+    tamFrame = (len(comando))
+    print("Tamanho do frame: ",tamFrame)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+    s.send(comando)
+    data = s.recv(BUFFER_SIZE)
+    s.close()
+    print(len(data))
+    return data
 
-TCP_PORT = 9000 #9762
+
+#TCP_IP = '10.238.10.108'
+TCP_IP = '10.238.10.107'
+
+TCP_PORT = 9762 #9762
 BUFFER_SIZE = 1024
-MESSAGE = bytearray(b'\x00\x0C\x0C')
-print("soma checksum: ",sum(MESSAGE[:-1]))
-tamFrame = (len(MESSAGE))
-print("Tamanho do frame: ",tamFrame)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
+#MESSAGE = bytearray(b'\x00\x0C\x0C')
+
 
 # --- Retorno
+retorno = enviarComando(b'\x53\x54\x58\x00\x03\x00\x5f\x5f\x45\x54\x58')
+#enviarComando(b'\x00\x03\x03')
+print ("retorno : ")
+print(retorno[2])
 
-data = s.recv(BUFFER_SIZE)
-s.close()
-
-# - ler data e hora
-
-opcao12(data)
+#opcao12(retorno)
