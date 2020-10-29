b911d3d952b4cc5e1941f022d47cfbfe0598d44c
Kleber Coelho <kleber.amg79@gmail.com>
2020-10-29 12:55:35 -0300

    add enviar.py

--- enviar.py
+++ enviar.py
@@ -1,89 +1,29 @@
 import socket
+import BitHandler as convert
 
 
-def bcd2int(bcd):
-    digit=0
-    ret=0
+def enviarComando(payload):
+    cabecalho = b'STX'
+    rodape = b'ETX'
+    frame = cabecalho + payload +rodape
 
-    digit = (bcd >> 4) & 0x0F
-    ret = ret * 10 + digit;
-
-    digit = bcd & 0x0F
-    ret = ret * 10 + digit
-    return ret
-
-def checksum(frameHex):
-    tamFrameHex = len(frameHex)
-    frameEnvioHex = frameHex
-    frameHex.append(0)
-
-
-    for i in range(tamFrameHex):
-        print(i)
-        frameEnvioHex[tamFrameHex] += frameHex[i]
-
-
-    return frameEnvioHex[tamFrameHex]
-
-
-#--- Ler Data e Hora
-def opcao12(data):
-
-    print ("received data:", data)
-    print("tamanho do retorno data: ", len(data))
-    print("tipo do dado recebido: ", type(data))
-    print("------------------------------------")
-    print("pos0: ",data[0])
-    print("pos1: ",data[1])
-    print("dia: ",data[2])
-    print("mes: ",bcd2int(data[3]))
-    print("ano: ",bcd2int(data[4]))
-    print("hora: ",bcd2int(data[5]))
-    print("min: ",bcd2int(data[6]))
-    print("seg: ",bcd2int(data[7]))
-    print("cs: ",data[8])
-    print("data: "+ str(bcd2int(data[2])) +"/" + str(bcd2int(data[3])) +"/"+ str(bcd2int(data[4]))+" "+str(bcd2int(data[5]))+":"+str(bcd2int(data[6])))
-
-#  MAIN ()
-
-# Conexao TCP
-
-def enviarComando(comando,payload):
-    #calcula checksum
-    cs = 0
-    for i in range(len(payload -1)):
-        cs = cs + payload[i]
-    cs =
-
-
-
-    FrameHex = comando + payload
-    print("soma checksum: ",sum(comando[:-1]))
-    tamFrame = (len(comando))
-    print("Tamanho do frame: ",tamFrame)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
-    s.send(comando)
+    print("tamanho   :",len(frame))
+    print("ENVIANDO  :", frame)
+    print("ENVIANDO HEX  :", convert.fmtByte_to_Str(frame))
+    s.send(frame)
     data = s.recv(BUFFER_SIZE)
     s.close()
     print(len(data))
-    print(data)
-    return data
+    print("data  retorno :", convert.fmtByte_to_Str(data))
 
 
-#cTCP_IP = '10.238.10.109'
-TCP_IP = '10.238.0.41'
 
-TCP_PORT = 9000 #9762
+TCP_IP = '10.238.10.111'
+#TCP_IP = '10.238.0.41'
+
+TCP_PORT = 9762
 BUFFER_SIZE = 1024
-#MESSAGE = bytearray(b'\x00\x0C\x0C')
 
 
-# --- Retorno
-retorno = enviarComando(b'\x53\x54\x58\x00\x03\x00\x5f\x5f\x45\x54\x58')
-#enviarComando(b'\x00\x03\x03')
-print ("retorno : ")
-print(retorno[2])
-
-
-#opcao12(retorno)
